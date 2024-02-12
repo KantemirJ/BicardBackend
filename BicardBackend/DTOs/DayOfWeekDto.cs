4ed16597b168e5c8930f8864b86cdfc2e493c931
Kantemir Zhaichibekov <116822401+KantemirJ@users.noreply.github.com>
2024-02-12 18:33:44 +0600

    SchedulesController created

---
+++ BicardBackend/DTOs/DayOfWeekDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BicardBackend.DTOs
+{
+    public class DayOfWeekDto
+    {
+        public string Name { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public int EmployeeId { get; set; }
+    }
+}
