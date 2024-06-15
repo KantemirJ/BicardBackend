2aa40f0cfd24b62fe9ff81cc4174e9d22eb39074
KantemirJ <kantemir.exe@gmail.com>
2024-06-15 19:05:29 +0600

    some fixes

---
+++ BicardBackend/Migrations/20240615095023_AddPhotoToAboutClinic.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BicardBackend.Migrations
+{
+    public partial class AddPhotoToAboutClinic : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PathToPhoto",
+                table: "AboutClinic",
+                newName: "PathToPhoto2");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PathToPhoto1",
+                table: "AboutClinic",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PathToPhoto1",
+                table: "AboutClinic");
+
+            migrationBuilder.RenameColumn(
+                name: "PathToPhoto2",
+                table: "AboutClinic",
+                newName: "PathToPhoto");
+        }
+    }
+}
